392029e980884aecd73b6bc9df72453bb39c5ebb
Eugene Soo <eugene@soo.sg>
2017-11-01 09:01:20 -0700

    fixed initial commit bugs

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 1337;
 
-app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.listen(port, () => {
   console.log(`(>^.^)> now listening on ${port}!`);
